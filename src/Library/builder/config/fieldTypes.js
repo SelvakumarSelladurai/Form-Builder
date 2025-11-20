0b8996d990ca9a9a55af040dd16c1da111c07612
SelvakumarBFX <selvakumar@bfxinfotech.com>
2025-11-20 15:03:17 +0530

    enhance : improve the function by importing the dnd

--- src/Library/builder/config/fieldTypes.js
+++ src/Library/builder/config/fieldTypes.js
@@ -1,6 +1,11 @@
 export const fieldTypes = [
   { type: "text", label: "Text Field" },
-  { type: "number", label: "Number Field" },
+  { type: "number", label: "Number" },
   { type: "date", label: "Date Field" },
   { type: "checkboxGroup", label: "Checkbox Group" },
+  { type: "radioGroup", label: "Radio Group" },
+  { type: "textarea", label: "Text Area" },
+  { type: "photo", label: "Photo" },
+  { type: "signature", label: "Signature" },
+  { type: "checkpoints", label: "Checkpoints" },
 ];

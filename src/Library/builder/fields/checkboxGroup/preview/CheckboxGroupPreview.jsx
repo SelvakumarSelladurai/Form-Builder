ef10ca4e27e3d76298b1f184f623080e58ad83be
SelvakumarBFX <selvakumar@bfxinfotech.com>
2025-11-21 12:57:03 +0530

    enhance : enhance the checkboxgroup and date preview

--- src/Library/builder/fields/checkboxGroup/preview/CheckboxGroupPreview.jsx
+++ src/Library/builder/fields/checkboxGroup/preview/CheckboxGroupPreview.jsx
@@ -1,19 +1,24 @@
 export default function CheckboxGroupPreview({ field }) {
-  const options =
-    field.options && field.options.length > 0
-      ? field.options
-      : [{ label: "Option 1" }, { label: "Option 2" }];
+  const options = field.options?.length
+    ? field.options
+    : [{ label: "Option 1" }];
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-1"> 
       {options.map((option, index) => (
-        <label key={index} className="flex items-start gap-2">
+        <label
+          key={index}
+          className="flex items-center gap-2 py-[2px]"
+        >
           <input
             type="checkbox"
-            className="checkbox checkbox-primary cursor-not-allowed"
+            className="checkbox checkbox-2xs" 
             readOnly
           />
-          <span>{option.label}</span>
+
+          <span className="text-[11px] text-gray-700 leading-none">
+            {option.label}
+          </span>
         </label>
       ))}
     </div>
